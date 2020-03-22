29c9e92c51c6c5f0efa0fdea316a5d48e67f07b4
ahmedhassan202025 <ahahmed202025@gmail.com>
2020-03-22 20:48:18 +0200

    3/22/2020

--- spotify/src/components/PremiumPage/PremuimComponent.js
+++ spotify/src/components/PremiumPage/PremuimComponent.js
@@ -42,29 +42,12 @@ class Premium extends Component {
   }
 
   handlePremium() {
-    // this.setState({ Premium: true });
-    // const post = {
-    //   Premium: true,
-    //   id: 2
-    // };
-    // this.props.PremiumPost(post);
-    // var myObject = JSON.stringify(this.props.id)
-
-  //   var count = Object.length.valueOf(myObject.us);
-  //   alert(count)
-  // var lastElement = this.props.id[this.props.id.length-1][0];
-
- 
-  this.setState({
-    tempId:this.props.id.id.length + 1
-  })
-   
-  //     this.setState({
-  //       tempId:map.size
-  //     })
+    this.setState({ Premium: true });
+    const post = {
+      Premium: true,
+      id: 2
+    };
     
-  // }))
-  // alert(this.state.tempId);
   }
 
   toggleNav() {
