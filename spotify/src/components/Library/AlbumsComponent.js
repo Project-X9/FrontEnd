ffc2752924022316dd4d9017c86571532761ec94
ahmedhassan202025 <ahahmed202025@gmail.com>
2020-04-01 06:30:37 +0200

    nothing changed 1

--- spotify/src/components/Library/AlbumsComponent.js
+++ spotify/src/components/Library/AlbumsComponent.js
@@ -27,7 +27,7 @@ class Albums extends Component {
                         </Col>
                     </Row>
                     <Row>
-                        <Col  md={12}>
+                        <Col  md={12} className="m-0">
                             <div className="gridView">
                                 <div className="CardsLibrary">
                                     <Row>
