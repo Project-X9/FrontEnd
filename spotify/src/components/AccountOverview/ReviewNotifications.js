7569d7c95ab9b8728ee79958433ea3a098732f53
Ahmed Hashish <ahmedhashish@ymail.com>
2020-06-07 01:58:40 +0200

    ReviewNotifications component now shows notifications

--- spotify/src/components/AccountOverview/ReviewNotifications.js
+++ spotify/src/components/AccountOverview/ReviewNotifications.js
@@ -1,75 +1,52 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import {
-  Card, CardBody, CardText, CardSubtitle,
-} from 'reactstrap';
-import { NavLink} from "react-router-dom";
 import {Button} from "reactstrap";
 /**
- * Class for the freePlan component which is a container shown for the free users
+ * Class for the reviewing the notifications 
  */
 class ReviewNotifications extends Component {
   /**
-   * Responsible for showing a container customized for free users
-   * @returns Components that will be display the container, and will be used in AccountOverview.js
+   * Responsible for showing a container containing all the notifications sent to the user
+   * @returns Components that will be display the notifications, and will be used in AccountOverview.js
    */
   render() {
+    let Notifications = "";
+    Notifications = this.props.data_be.data_be.notifications.map((notification) => {
+      return(
+        <div key= {notification._id} className="ReadNotificationsRow">>
+          <div className="row">
+            <div className="col-9">
+              <h5 className="ReadNotificationsEvents"> 
+                {notification.event} 
+              </h5>
+            </div>
+            <div className="col-3"> 
+              {notification.read===false?(
+                  <div>
+                    <Button className="ReadNotificationsTrueButton">
+                      Not Read
+                    </Button>
+                  </div>
+                  ):(
+                  <div className="ReadNotificationsFalse">
+                    <h5>
+                      Read
+                    </h5>
+                  </div>
+                )} 
+            </div>
+          </div>
+          <hr />
+        </div>
+      )
+    })
     return (
-        <div>
+      <div>
         <div className="row">
           <h1 className="BigHeader">Notifications</h1>
         </div>
-
-       
-        <div className="row">
-          <div className="col Content1">
-            <h5>Username</h5>
-          </div>
-          <div className="col Content2">
-            <h5> {this.props.data_be.data_be.name} </h5>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col Content1">
-            <h5>Email</h5>
-          </div>
-          <div className="col Content2">
-            <h5>{this.props.data_be.data_be.email}</h5>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col Content1">
-            <h5>Age</h5>
-          </div>
-          <div className="col Content2">
-            <h5>{this.props.data_be.data_be.age}</h5>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
-          <div className="col Content1">
-            <h5>Country</h5>
-          </div>
-          <div className="col Content2">
-            <h5>EG</h5>
-          </div>
-        </div>
-        <hr />
-       
-       
-        <div className="row">
-          <Button onClick={() => {
-            this.handleLogout();
-          }}
-            className="EditProfile" color="success">
-            <NavLink className="InsideEditProfile" to="/">
-              SIGN OUT
-            </NavLink>
-          </Button>
-        </div>
+        {Notifications}
       </div>
     );
   }
